125d2ba4d35a0f6ef4109719dd90c01cf4757e65
Juan Pablo Sosa <jpasosa@gmail.com>
2017-08-17 17:50:31 -0300

    install GrahamCampbell/Laravel-Markdown

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        GrahamCampbell\Markdown\MarkdownServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -226,6 +227,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
         'Access'    => Styde\Html\Facades\Access::class,
+        'Markdown'  => GrahamCampbell\Markdown\Facades\Markdown::class,
 
     ],
 
