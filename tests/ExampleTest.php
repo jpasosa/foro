f2b0fbaddc787f6863b02c7cdcd0c104811abcc1
Juan Pablo Sosa <jpasosa@gmail.com>
2017-08-18 11:23:38 -0300

    Prepare for TDD test-driven development, creamos base paralela de pruebas y configuramos para ir desarrollando con pruebas . . Excelente!

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,6 +6,11 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    
+    
+    use DatabaseTransactions;
+    
+
     /**
      * A basic functional test example.
      *
@@ -13,7 +18,22 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel');
+
+
+
+        $name_user = 'Duilio Palacios';
+
+
+        $user = factory(\App\User::class)->create([
+            'name' => $name_user,
+            'email' => 'palacios@gmail.com',
+            ]);
+
+
+        $this->actingAs( $user, 'api' );
+
+
+        $this->visit('api/user')
+             ->see($name_user);
     }
 }
