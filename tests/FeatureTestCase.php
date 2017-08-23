ee99e6e3f71ee1a1750d1ed248eb5cb1d2649582
Juan Pablo Sosa <jpasosa@gmail.com>
2017-08-23 17:55:00 -0300

    add functionality to test posts, refactorizando código

--- tests/FeatureTestCase.php
+++ tests/FeatureTestCase.php
@@ -11,7 +11,17 @@ class FeatureTestCase extends TestCase
 	use DatabaseTransactions;
 
 
+	function seeErrors( array $errors )
+	{
 
+		foreach ($errors as $column => $error)
+		{
+			foreach (array($error) AS $k => $message) {
+				$this->seeInElement("#field_$column .help-block", $message);
+			}
+		}
+
+	}
 
 
 
